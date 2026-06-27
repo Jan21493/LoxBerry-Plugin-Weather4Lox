8e217b7cfcede60b6b1fd1a43d46bfd3cfffd9af
Jan Wachsmuth <jan.wachsmuth@web.de>
2026-06-27 12:14:48 +0200

    Minor fixes

--- preupgrade.sh
+++ preupgrade.sh
@@ -12,7 +12,7 @@ mkdir -p /tmp/$ARGV1\_upgrade
 mkdir -p /tmp/$ARGV1\_upgrade/config
 mkdir -p /tmp/$ARGV1\_upgrade/log
 mkdir -p /tmp/$ARGV1\_upgrade/themes
-chown loxberry:loxberry $ARGV5/log/plugins/$ARGV3/*Emulator.log
+
 if [ -f $ARGV5/config/plugins/$ARGV3/cloudemu_state ]; then
     echo "<INFO> Removing existing cloudemu_state file"
     rm $ARGV5/config/plugins/$ARGV3/cloudemu_state
